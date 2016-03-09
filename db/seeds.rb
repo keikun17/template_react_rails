4843b6685c7b69242cd852dcb2d08c1961952016
Buddy Magsipoc <keikun17@gmail.com>
2016-03-10 00:06:42 +0800

    seed。

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+game_names = [
+  'CounterStrike','Diablo 3', 'Starcraft 2', 'Heroes of the Storm', "Overwatch"
+]
+
+game_names.each do |game_name|
+  Game.find_or_create_by(name: game_name)
+end
