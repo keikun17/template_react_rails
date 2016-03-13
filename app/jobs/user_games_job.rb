c316a563217c7d7c053af6d4818abe97e1532be7
Buddy Magsipoc <keikun17@gmail.com>
2016-03-13 09:44:08 +0800

    GASP!。

--- app/jobs/user_games_job.rb
+++ app/jobs/user_games_job.rb
@@ -2,12 +2,10 @@ class UserGamesJob < ApplicationJob
   queue_as :default
 
   def perform(user)
-    binding.remote_pry
     ActionCable.server.broadcast 'user_games-#{user.id}', games: user.games
   end
 
   def render_games_list(games)
-    binding.remote_pry
     react_component('Something', props: games)
     # ApplicationController.renderer.render(partial: 'events/event', locals: { event: event })
   end
