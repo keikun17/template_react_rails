c568d6faa40c36eea09077537e7748add0296643
Buddy Magsipoc <keikun17@gmail.com>
2016-04-12 16:48:55 +0800

    Remove timestamp thing。

--- client/app/bundles/GamesForm/reducers/gamesFormReducer.jsx
+++ client/app/bundles/GamesForm/reducers/gamesFormReducer.jsx
@@ -12,7 +12,6 @@ export default function gamesFormReducer($$state = $$initialState, action) {
   switch (type) {
     case actionTypes.GAMES_FORM_GAMES_ADD:
       // return $$state.set('current_games', current_games.concat(game))
-      var timestamp = Date.now(games)
       App.user_games.add_game({game: data.game,
                               cb_action: actionTypes.GAMES_FORM_GAMES_ADD
       })
