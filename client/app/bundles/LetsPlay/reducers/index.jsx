91c73e09ba98df1d6ea5f1a099c8619999952330
Buddy Magsipoc <keikun17@gmail.com>
2016-04-15 22:50:39 +0800

    Fix。

--- client/app/bundles/LetsPlay/reducers/index.jsx
+++ client/app/bundles/LetsPlay/reducers/index.jsx
@@ -1,8 +1,8 @@
 // This file is our manifest of all reducers for the app.
 // See also /client/app/bundles/LetsPlay/store/LetsPlayStore.jsx
 // A real world app will likely have many reducers and it helps to organize them in one file.
-import gamesSelectReducer from './gamesSelectReducer';
-import { $$initialState as $$letsPlayState } from './gamesSelectReducer';
+import gameSelectReducer from './gameSelectReducer';
+import { $$initialState as $$letsPlayState } from './gameSelectReducer';
 
 export default {
   $$letsPlayStore: gameSelectReducer,
